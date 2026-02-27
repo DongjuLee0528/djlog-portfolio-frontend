9dfceeb7d39cdde9c0d4af4fa4bb846394b0eaac
DongjuLee <dozoo0528@gmail.com>
2026-02-28 02:30:35 +0900

    refactor: 포트폴리오 사이트 코드 품질 전면 개선

---
+++ src/constants/contact.ts
@@ -0,0 +1,15 @@
+// 연락처 및 개인정보 상수
+export const CONTACT_INFO = {
+  EMAIL: 'hello@nexbot.dev',
+  GITHUB: 'https://github.com/nexbot-dev',
+  LINKEDIN: 'https://linkedin.com/in/nexbot-dev',
+  TWITTER: 'https://twitter.com/nexbot_dev',
+} as const;
+
+export const SITE_INFO = {
+  NAME: 'NexBot Portfolio',
+  TITLE: 'NexBot - 풀스택 개발자',
+  DESCRIPTION: '혁신적인 웹 솔루션을 만드는 풀스택 개발자입니다.',
+  URL: 'https://nexbot.dev',
+  AUTHOR: 'NexBot',
+} as const;
