b52f8e5c4763868f62990f70e4030e797a8f8155
DongjuLee <dozoo0528@gmail.com>
2026-03-04 19:25:19 +0900

    refactor: 포트폴리오 사이트 코드 품질 전면 개선

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+import { Toaster } from 'react-hot-toast';
+import { ErrorBoundary } from './components/common';
 
 // 페이지 컴포넌트 import
 import Home from './pages/Home';
@@ -24,22 +26,24 @@ const ScrollToTop = () => {
 
 function App() {
   return (
-    <Router>
-      <ScrollToTop />
-      <div className="bg-[#F7F7F7] min-h-screen text-[#333333] selection:bg-[#4A90E2] selection:text-white">
-        <AnimatePresence mode="wait">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/project/:id" element={<ProjectDetail />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/admin" element={<Admin />} />
-            {/* 404 페이지 라우트 (가장 마지막에 위치) */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AnimatePresence>
-      </div>
-    </Router>
+    <ErrorBoundary>
+      <Router>
+        <ScrollToTop />
+        <div className="bg-[#F7F7F7] min-h-screen text-[#333333] selection:bg-[#4A90E2] selection:text-white">
+          <AnimatePresence mode="wait">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/project/:id" element={<ProjectDetail />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </AnimatePresence>
+        </div>
+        <Toaster />
+      </Router>
+    </ErrorBoundary>
   );
 }
 
