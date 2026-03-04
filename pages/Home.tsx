60bf698ad7a0f102a68329c67cafeb5702947599
DongjuLee <dozoo0528@gmail.com>
2026-03-04 19:26:21 +0900

    refactor: 포트폴리오 사이트 코드 품질 전면 개선

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,16 +1,16 @@
 // 메인 홈 페이지 컴포넌트
-import React from 'react';
+import React, { memo } from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Projects from '../components/Projects';
 import Stack from '../components/Stack';
 import Contact from '../components/Contact';
 
-const Home: React.FC = () => {
+const Home: React.FC = memo(() => {
   return (
     <>
       <Navbar />
-      <main>
+      <main role="main" aria-label="메인 콘텐츠">
         <Hero />
         <Projects />
         <Stack />
@@ -18,6 +18,9 @@ const Home: React.FC = () => {
       <Contact />
     </>
   );
-};
+});
+
+// displayName 설정으로 디버깅 시 컴포넌트 식별 용이
+Home.displayName = 'Home';
 
 export default Home;
