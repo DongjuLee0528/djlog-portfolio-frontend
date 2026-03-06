90ada41f9b5429b08b7cd43e80f1ee9112d6d0ba
DongjuLee <dozoo0528@gmail.com>
2026-03-06 16:40:20 +0900

    refactor: 포트폴리오 사이트 코드 품질 전면 개선

--- components/common/index.ts
+++ components/common/index.ts
@@ -1,3 +1,15 @@
+/**
+ * 공통 컴포넌트 모듈 내보내기
+ *
+ * 재사용 가능한 UI 컴포넌트들을 중앙에서 관리하고 내보냅니다.
+ * 다른 컴포넌트에서 간편하게 import할 수 있도록 배럴 패턴을 사용합니다.
+ */
+
+/** 로딩 상태를 표시하는 스피너 컴포넌트 */
 export { LoadingSpinner } from './LoadingSpinner';
+
+/** React 에러 경계를 처리하는 컴포넌트 */
 export { ErrorBoundary } from './ErrorBoundary';
+
+/** 모달 다이얼로그 컴포넌트 */
 export { Modal } from './Modal';
